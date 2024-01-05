17e6f8450b6a3390dbe4410cac4cdb44976bb605
ivsouza <ivanildo.jnr@outlook.com>
2024-01-05 17:39:22 -0300

    temp removed

--- API-ETL/pandas/awesome_data_ingestion/src/main.py
+++
@@ -1,6 +0,0 @@
-# import ELWithPandas as pipeline
-from awesome_data_ingestion.ExtractAndLoadWithPandas import ELWithPandas as pipeline
-
-dic =  pipeline.AwesomeAPI().Currency.GetData("EUR", "BRL")
-
-print(dic)
