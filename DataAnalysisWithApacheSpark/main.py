8ec8350a16d3235ceeb320d4911d5c9afa203e37
ivsouza <ivanildo.souza@mercadolivre.com>
2024-01-05 17:58:11 -0300

    feature API_ETL

--- DataAnalysisWithApacheSpark/main.py
+++
@@ -1,9 +0,0 @@
-from data_explorer.SparkInstance import GetOrCreateSession as SparkSession
-
-my_spark_session = SparkSession()
-
-df = my_spark_session.read.csv("./train.csv")
-
-print("Essa é a quantidade de linhas do dataframe")
-print(df.count())
-
--- DockerContainers/airflow/dags/data_raw/insert_table_clients.sql
+++
@@ -1,4 +0,0 @@
-insert into data_raw.clients values('randomid', 'cristiane_barauna');
-insert into data_raw.clients values('randomid', 'ivanildo_barauna');
-insert into data_raw.clients values('randomid', 'benaia_rangel_barauna');
-insert into data_raw.clients values('randomid', 'azael_rangel_barauna');
