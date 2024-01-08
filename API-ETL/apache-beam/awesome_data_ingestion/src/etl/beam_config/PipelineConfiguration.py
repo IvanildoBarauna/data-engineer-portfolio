f64f614ab8ae2f34eb0f25deb59c22b8f90c20c5
ivsouza <ivanildo.souza@mercadolivre.com>
2024-01-08 15:53:25 -0300

    feature: extract routine + tests

--- API-ETL/apache-beam/awesome_data_ingestion/src/etl/beam_config/PipelineConfiguration.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/etl/beam_config/PipelineConfiguration.py
@@ -4,14 +4,15 @@ def DefaultPipeConfig():
 
     import logging
 
-    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
-    
+    logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+
     try:
         pipe_options = PipelineOptions()
         pipe_options.view_as(
-            beam.options.pipeline_options.SetupOptions).save_main_session = True
+            beam.options.pipeline_options.SetupOptions
+        ).save_main_session = True
         logging.info("Pipeline configuration DONE")
     except Exception as err:
         logging.error(f"Pipeline error configuration {err}")
-    
+
     return pipe_options
--- API-ETL/apache-beam/awesome_data_ingestion/src/etl/extract.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/etl/extract.py
@@ -1,35 +1,69 @@
 import apache_beam as beam
-from etl.beam_config.PipelineConfiguration import DefaultPipeConfig
+from beam_config.PipelineConfiguration import DefaultPipeConfig
 import requests as req
 from datetime import datetime
+import logging
+
 
 def APIToParquet(endpoint: str):
-    
+    import pyarrow
+
     response = req.get(endpoint)
 
     pipe_options = DefaultPipeConfig()
     dic = response.json()
 
-    output_path = "../../data/external/"
+    output_path = "/Users/ivsouza/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/external/kaggle/"
 
-    def ts() -> str:
+    def CurrentTimestampStr() -> str:
         current = datetime.now().timestamp()
-        return current.__str__().replace(".", "")
+        return str(int(current))
+
+    def ParquetSchemaLoad(element: dict):
+        import pyarrow
+
+        try:
+            api_header = list(element.keys())
+
+            schema = []
+
+            for field in api_header:
+                schema += [(field, pyarrow.string())]
+
+            beam_schema = pyarrow.schema(schema)
+
+            logging.info("Schema OK")
+
+            return beam_schema
+
+        except Exception as Err:
+            logging.error(f"Schema Error >>>>> {Err}")
 
     def dict_to_values_list(element):
-            return list(element.values())
+        return list(element.values())
 
-    with beam.Pipeline(options=pipe_options) as pipe:
-        input_pcollection = (
-            pipe
-            | "Create" >> beam.Create([dic])
-            | "ValuesList" >> beam.Map(dict_to_values_list)
-            | "WriteToCSV" >> beam.io.WriteToText(
-                f'{output_path}_dolar_today_{ts()}'
-                , file_name_suffix=".csv"
-                ## Passar o header como variável, recuperando os keys json retornado
-                , header="created_at,parity,friendly_name,price,day_over_day,day_over_day_pct,higher,lower"
-                ,num_shards=1
-                , shard_name_template="")
-        )
+    try:
+        # logging.info("Iniciando geração de arquivo .avro")
+        # SchemaLoaded = AvroSchemaLoad(element=dic)
+        FileSchema = ParquetSchemaLoad(dic)
+        logging.info("Iniciando pipeline")
+        with beam.Pipeline(options=pipe_options) as pipe:
+            input_pcollection = (
+                pipe
+                | "Create" >> beam.Create([dic])
+                # | "ExtractHeader" >> beam.Map(extract_header)
+                | "ValuesList" >> beam.Map(dict_to_values_list)
+                # | "PrintData" >> beam.ParDo(lambda element: print(element))
+                | "WriteToParquet"
+                >> beam.io.WriteToParquet(
+                    filename=f"{output_path}dolar_today_{CurrentTimestampStr()}.parquet",
+                    schema=FileSchema
+                    ## Passar o header como variável, recuperando os keys json retornado
+                )
+            )
+        logging.info("Pipeline executado com sucesso.")
+    except Exception as err:
+        logging.error(f"Erro ao executar o pipeline: {err}")
 
+
+APIToParquet("https://economia.awesomeapi.com.br/last/USD-BRL")
