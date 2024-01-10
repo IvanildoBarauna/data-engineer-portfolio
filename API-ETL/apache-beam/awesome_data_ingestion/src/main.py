41b5b15d453a7715c2a7f37081743300841ecdff
ivsouza <ivanildo.souza@mercadolivre.com>
2024-01-10 19:48:37 -0300

    feature: main

--- API-ETL/apache-beam/awesome_data_ingestion/src/main.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/main.py
@@ -4,5 +4,8 @@ import os
 api = "https://economia.awesomeapi.com.br/last/USD-BRL"
 path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/external/kaggle/"
 
+
 NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
 NewExtract.PipelineRun()
+
+# print(os.get_exec_path())
--- API-ETL/apache-beam/awesome_data_ingestion/tests/testes_etl.py
+++ API-ETL/apache-beam/awesome_data_ingestion/tests/testes_etl.py
@@ -1,1 +1,5 @@
-import pyarrow
+import pyarrow.parquet as pq
+
+file_path = "/Users/ivsouza/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/external/kaggle/dolar_today_1704919030-00000-of-00001.parquet"
+
+pq.read_table(file_path)
