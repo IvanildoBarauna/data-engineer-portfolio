41aa656838931e278aad5b460f5f28c37a4474fc
ivbarauna <ivanildo.jnr@outlook.com>
2024-01-11 19:09:22 -0300

    feature: main + notebook

--- API-ETL/apache-beam/awesome_data_ingestion/src/main.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/main.py
@@ -1,18 +1,9 @@
 from etl.extract import ExtractDataAPI
 import os
 
-# import requests
-
-api = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+api = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,BTC-USD,ETH-USD"
 path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/external/kaggle/"
 
 NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
 
 NewExtract.PipelineRun()
-
-# response = NewExtract.APIToDicionary()
-# json_data = response["responseData"]
-# params = response["params"]
-
-# for item in enumerate(params):
-#     print(item)
