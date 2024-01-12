fb4abca7a418b0b51b46d0a843aa7c83bd959479
ivsouza <ivanildo.souza@mercadolivre.com>
2024-01-12 15:14:36 -0300

    feat: CTA

--- API-ETL/apache-beam/awesome_data_ingestion/src/main.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/main.py
@@ -1,8 +1,9 @@
+from hmac import new
 from etl.extract import ExtractDataAPI
 from etl.transform import TransformAPIData
 import os
 
-api = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL"
+api = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-USD,BTC-BRL"
 path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/raw/kaggle/"
 
 NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
