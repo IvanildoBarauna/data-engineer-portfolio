69c0da6e23a1e9cf39fd92f2166adfc7f8af876d
ivsouza <ivanildo.souza@mercadolivre.com>
2024-01-12 18:05:05 -0300

    feature: testes

--- API-ETL/apache-beam/awesome_data_ingestion/src/main.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/main.py
@@ -1,13 +1,13 @@
-from hmac import new
+
 from etl.extract import ExtractDataAPI
 from etl.transform import TransformAPIData
 import os
 
-api = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-USD,BTC-BRL"
-path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/raw/kaggle/"
+api = "https://economia.awesomeapi.com.br/last/USD-BRL,ETH-USD,BTC-USD,USDL-BRL"
+path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/raw/"
 
 NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
 NewExtract.PipelineRun()
 
-NewTransform = TransformAPIData(ExtractedFiles=NewExtract.ExtractedFilePath)
-NewTransform.PipelineRun()
+# NewTransform = TransformAPIData(ExtractedFiles=NewExtract.ExtractedFilePath)
+# NewTransform.PipelineRun()
--- API-ETL/apache-beam/awesome_data_ingestion/tests/testes_etl.py
+++ API-ETL/apache-beam/awesome_data_ingestion/tests/testes_etl.py
@@ -1,5 +1,48 @@
-import pyarrow.parquet as pq
+import requests
 
-file_path = "/Users/ivsouza/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/external/kaggle/dolar_today_1705005057-00000-of-00001.parquet"
+api = "https://economia.awesomeapi.com.br/last/USD-BRL"
 
-pq.read_table(file_path)
+
+class ExtractDataAPI:
+    def __init__(self, endpoint: str, output_path: str) -> None:
+        self.endpoint = endpoint
+        self.output_path = output_path
+        self.ExtractedFilePath = []
+        self.ValidParams = []
+
+    def APIToDicionary(self):
+            def ValidListOfParams():
+                arr_endpoint = self.endpoint.split("/")
+                params = arr_endpoint[len(arr_endpoint) - 1]
+                lstParams = params.split(",")
+                
+                list_of_avaliable = requests.get("https://economia.awesomeapi.com.br/json/available").json()
+                
+                ValidParams = []
+                
+                for param in lstParams:
+                    if param in list_of_avaliable:
+                        ValidParams.append(param.replace("-", ""))
+                    else:
+                        print(f"Param: {param} is not valid for call")
+                
+                return ValidParams
+            ## Se for válido segue
+            
+            
+            validParams = ValidListOfParams()
+            if validParams:
+                print(f"Parameters OK >>> {validParams}")
+                response = requests.get(self.endpoint)
+                if response.ok:
+                    print(f"Response OK >>> {response}")
+                    return dict(responseData=response.json(), params=validParams)
+                else:
+                    print(f"Response failed >>> {response}")
+
+
+api = "https://economia.awesomeapi.com.br/last/USD-BRL"
+path = ""
+
+NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
+NewExtract.APIToDicionary()
