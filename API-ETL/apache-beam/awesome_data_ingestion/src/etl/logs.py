edf864a0c050334dc11115a142ef53d3453022e0
ivsouza <ivanildo.souza@mercadolivre.com>
2024-01-12 18:04:32 -0300

    feat: logs level custom

--- API-ETL/apache-beam/awesome_data_ingestion/src/etl/logs.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/etl/logs.py
@@ -1,16 +1,29 @@
-from apache_beam import logging
+import logging
 
+# Configurar o logger
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s -  %(message)s"
+    level=logging.CRITICAL,  # Defina o nível globalmente como CRITICAL
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'  # Formato da data e hora
 )
 
+# Criar um logger para cada nível desejado
+logger_error = logging.getLogger('error_logger')
+logger_warn = logging.getLogger('warn_logger')
+logger_info = logging.getLogger('info_logger')
 
-def ConsoleInfo(msg: str):
-    return logging.info(msg)
+# Configurar os níveis desejados para cada logger
+logger_error.setLevel(logging.ERROR)
+logger_warn.setLevel(logging.WARNING)
+logger_info.setLevel(logging.INFO)
 
 
-def WarningInfo(msg: str):
-    logging.basicConfig(
-        level=logging.WARNING, format="%(asctime)s - %(levelname)s -  %(message)s"
-    )
-    return logging.info(msg)
+def ConsoleInfo(msg):
+    return logger_info.info(msg)
+
+def ConsoleError(msg):
+    return logger_error.error(msg)
+
+def ConsoleWarning(msg):
+    return logger_warn.warning(msg)
+
