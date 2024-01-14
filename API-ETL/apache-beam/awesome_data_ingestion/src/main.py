8e7b6f9b014574483500e00cce243ca0daddb8e3
Ivanildo Barauna de Souza Junior (Pessoal) <ivanildo.jnr@outlook.com>
2024-01-13 23:39:55 -0300

    feat: transform routine

--- API-ETL/apache-beam/awesome_data_ingestion/src/main.py
+++ API-ETL/apache-beam/awesome_data_ingestion/src/main.py
@@ -1,4 +1,3 @@
-
 from etl.extract import ExtractDataAPI
 from etl.transform import TransformAPIData
 import os
@@ -6,8 +5,8 @@ import os
 api = "https://economia.awesomeapi.com.br/last/USD-BRL,ETH-USD,BTC-USD,USDL-BRL"
 path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/raw/"
 
-NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
-NewExtract.PipelineRun()
+# NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
+# NewExtract.PipelineRun()
 
-# NewTransform = TransformAPIData(ExtractedFiles=NewExtract.ExtractedFilePath)
-# NewTransform.PipelineRun()
+NewTransform = TransformAPIData(FolderFiles=path)
+NewTransform.PipelineRun()
--- API-ETL/apache-beam/awesome_data_ingestion/tests/testes_etl.py
+++ API-ETL/apache-beam/awesome_data_ingestion/tests/testes_etl.py
@@ -1,48 +1,20 @@
-import requests
-
-api = "https://economia.awesomeapi.com.br/last/USD-BRL"
+import psycopg2
 
 
-class ExtractDataAPI:
-    def __init__(self, endpoint: str, output_path: str) -> None:
-        self.endpoint = endpoint
-        self.output_path = output_path
-        self.ExtractedFilePath = []
-        self.ValidParams = []
+connection_params = {
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'DW',
+    'user': 'SVC_DW',
+    'password': 'SVC_DW'}
 
-    def APIToDicionary(self):
-            def ValidListOfParams():
-                arr_endpoint = self.endpoint.split("/")
-                params = arr_endpoint[len(arr_endpoint) - 1]
-                lstParams = params.split(",")
-                
-                list_of_avaliable = requests.get("https://economia.awesomeapi.com.br/json/available").json()
-                
-                ValidParams = []
-                
-                for param in lstParams:
-                    if param in list_of_avaliable:
-                        ValidParams.append(param.replace("-", ""))
-                    else:
-                        print(f"Param: {param} is not valid for call")
-                
-                return ValidParams
-            ## Se for válido segue
-            
-            
-            validParams = ValidListOfParams()
-            if validParams:
-                print(f"Parameters OK >>> {validParams}")
-                response = requests.get(self.endpoint)
-                if response.ok:
-                    print(f"Response OK >>> {response}")
-                    return dict(responseData=response.json(), params=validParams)
-                else:
-                    print(f"Response failed >>> {response}")
-
-
-api = "https://economia.awesomeapi.com.br/last/USD-BRL"
-path = ""
-
-NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
-NewExtract.APIToDicionary()
+    # Conecte-se ao PostgreSQL
+with psycopg2.connect(**connection_params) as conn:
+    # Abra um cursor para executar comandos SQL
+    with conn.cursor() as cursor:
+        # Exemplo de inserção de dados (substitua isso com sua lógica específica)
+        query = """ 
+        INSERT INTO "STG".teste(teste) VALUES (2);
+        """
+        # values = (element['coluna1'], element['coluna2'])
+        cursor.execute(query)
